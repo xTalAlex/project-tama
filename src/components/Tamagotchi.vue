a09a8503f42cdc268ac6f354615a0459cbf16702
Alessandro Talamona <info@talale.it>
2026-08-08 19:58:07 +0200

    tailwindcss and phavuer install

---
+++ src/components/Tamagotchi.vue
@@ -0,0 +1,12 @@
+<template>
+  <Game :config="gameConfig">
+    <Scene name="MainScene">
+      <Rectangle :x="184" :y="184" :width="64" :height="64" :fillColor="0xAAAAAA" />
+    </Scene>
+  </Game>
+</template>
+
+<script setup>
+    import { Game, Scene, Rectangle } from 'phavuer'
+    const gameConfig = { width: 368, height: 368 }
+</script>
